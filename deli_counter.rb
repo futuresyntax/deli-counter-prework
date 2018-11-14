38f3e3b1ee6cad42b5741048bc8646e15d66bec0
FutureScript <fernandobaxter1@gmail.com>
2018-11-14 19:21:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,16 +6,20 @@ def line(katz_deli)
    katz_deli.each_with_index do |value, index|
      message += " #{index.to_i+1}. #{value}"
    end
-  
-
      puts "#{message}"
    end
 
 end
 
 def take_a_number(katz_deli, name)
-  puts name, katz_deli +1
+  katz_deli << name 
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def take_a_number(katz_deli, name)
+#   katz_deli << name
+#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+# end
 
 def now_serving(name)
   puts katz_deli.shift
@@ -23,6 +27,8 @@ def now_serving(name)
     puts "There is nobody waiting to be served!"
   end
 end
+
+
 
 
 
