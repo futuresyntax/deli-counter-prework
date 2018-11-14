cc2b92fd52a46420dd07e2944c692cc91f80ca75
FutureScript <fernandobaxter1@gmail.com>
2018-11-14 19:05:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,7 @@ def line(katz_deli)
    katz_deli.each_with_index do |value, index|
      message += " #{index.to_i+1}. #{value}"
    end
-    # message = "The line is currently:"
-    # katz_deli.each_with_index do |value, index|
-    #    message += " #{index.to_i+1}. #{value}"
-    #  end
+  
 
      puts "#{message}"
    end
