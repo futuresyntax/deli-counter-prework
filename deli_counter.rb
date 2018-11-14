58794255fbf61e5bec778832ecaa8244cb408644
FutureScript <fernandobaxter1@gmail.com>
2018-11-14 19:27:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,11 +16,6 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-#def take_a_number(katz_deli, name)
-#   katz_deli << name
-#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-# end
-
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
@@ -29,12 +24,7 @@ def now_serving(katz_deli)
 end
 end 
 
-#def now_serving(katz_deli)
-#   if katz_deli.length == 0
-#     puts "There is nobody waiting to be served!"
-#   else
-#     puts "Currently serving #{katz_deli.shift}."
-#   end
+
 
 
 
